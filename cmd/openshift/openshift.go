da51079f64b075f37a9b1303cc245de7eb377904
Mrunal Patel <mrunalp@gmail.com>
2015-03-10 00:18:04 +0000

    Adds reaping capability to openshift.

--- cmd/openshift/openshift.go
+++ cmd/openshift/openshift.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 
+	"github.com/golang/glog"
 	"github.com/openshift/origin/pkg/cmd/openshift"
 	"github.com/openshift/origin/pkg/cmd/util/serviceability"
 )
@@ -14,6 +17,30 @@ func main() {
 
 	basename := filepath.Base(os.Args[0])
 	command := openshift.CommandFor(basename)
+
+	// Launch a reaper routine if we are pid 1 for e.g. openshift-router in a container.
+	if os.Getpid() == 1 {
+		go func() {
+			sigs := make(chan os.Signal, 1)
+			signal.Notify(sigs, syscall.SIGCHLD)
+			for {
+				sig := <-sigs
+				glog.V(4).Infof("Signal received: %+v", sig)
+				glog.V(4).Infof("Launching reaper")
+				for {
+					// Reap zombies
+					glog.V(4).Infof("Waiting to reap")
+					cpid, err := syscall.Wait4(-1, nil, 0, nil)
+					if err == syscall.ECHILD {
+						glog.V(4).Infof("Received: %+v", err)
+						break
+					}
+					glog.V(4).Infof("Reaped process with pid %d", cpid)
+				}
+			}
+		}()
+	}
+
 	if err := command.Execute(); err != nil {
 		os.Exit(1)
 	}
